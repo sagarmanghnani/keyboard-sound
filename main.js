ac0b050365cffca0a0a60e09d15e08c427bd171a
sagar <sagarmanghnani@gmail.com>
2019-09-07 17:08:24 +0530

    added sounds to keys

---
+++ main.js
@@ -0,0 +1,39 @@
+"use strict";
+let a = document.getElementById("A");
+document.onkeypress = (ev) => {
+   let keyboardKey = String.fromCharCode(ev.keyCode);
+   keyboardKey = keyboardKey.toLowerCase();
+   switch(keyboardKey) {
+       
+       case 'a': {
+            var playA = new Audio("http://carolinegabriel.com/demo/js-keyboard/sounds/047.wav");
+            playA.play();
+            break;
+       }
+
+       case 's': {
+           var playS = new Audio("http://carolinegabriel.com/demo/js-keyboard/sounds/040.wav");
+           playS.play();
+           break;
+       }
+
+       case 'd': {
+           var playD = new Audio("http://carolinegabriel.com/demo/js-keyboard/sounds/041.wav");
+           playD.play();
+           break;
+       }
+
+       case 'f': {
+           var playF = new Audio("http://carolinegabriel.com/demo/js-keyboard/sounds/042.wav");
+           playF.play();
+           break;
+       }
+
+       case 'g': {
+           var playG = new Audio("http://carolinegabriel.com/demo/js-keyboard/sounds/046.wav");
+           playG.play();
+           break;
+       }
+   }
+}
+
